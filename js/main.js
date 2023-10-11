0bd9edcc27cf3d74f48a655a482bf242e919c0d2
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-10-11 23:27:08 +0500

    Add flexibility styles to the header section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+const headerMenuOpen = document.querySelector('.header__menu--open');
 
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 0) {
@@ -7,4 +9,8 @@ window.addEventListener('scroll', () => {
     else {
         header.classList.remove('shadow');
     }
+})
+
+headerMenuOpen.addEventListener('click', () => {
+    nav.classList.add('sidebar');
 })
